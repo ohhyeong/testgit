b876ffefc3dc3c599f08e19f7a028598dfea9f17
HyeongSuk <ohs878.oh@snucad.sun.ac.kr>
2025-11-26 15:31:59 +0900

    git_cmd.sh file added

---
+++ git_cmd.sh
@@ -0,0 +1,18 @@
+# 최초 1회: 사용자 정보
+git config --global user.name "HyeongSuk"
+git config --global user.email "ohs878.oh@snucad.sun.ac.kr"
+
+# 저장소 클론
+cd ./
+git clone https://github.com/ohhyeong/testgit.git
+ls ./testgit
+
+
+echo "you can use depository with git command as follow"
+echo "git fetch ;; compare local working directory to remote repository"
+echo "git pull ;; keep up to date this repository"
+echo "git status ;; check modified files"
+echo "git add .  ;; Modified files are upload to staging area before COMMIT"
+echo "git commit -m \"any comment\ ;; Files are move to repository before PUSH"" 
+echo "git branch ;; Check branch name before PUSH"
+echo "git push origin master"
